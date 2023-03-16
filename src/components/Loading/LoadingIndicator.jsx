9e0f11d27380fd54b8a2db5084bbbc6161b66d6e
Siddhi <bandivdekarsiddhi@gmail.com>
2023-03-16 18:00:42 +0530

    Loading Indicator spinkit

--- src/components/Loading/LoadingIndicator.jsx
+++ src/components/Loading/LoadingIndicator.jsx
@@ -4,6 +4,7 @@ import "./LoadingIndicator.css";
 const LoadingIndicator = () => {
   return (
     <div className="loading-container">
+      
       <div className="sk-chase">
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
